83f858077645e92c5c81cf1eaf31a360e57ac1d9
lokimetasmith <lokimail@gmail.com>
2023-09-12 16:10:29 -0500

    merged in code from geoff
    Signed-off-by: lokimetasmith <lokimail@gmail.com>
    
    moved pin definitions to stage2_hal.h
    
    swapped out stale PIRDS and PIRCS libs and swapped out pubinv PIRCS with mine until bug gets patched
    
    Signed-off-by: lokimetasmith <lokimail@gmail.com>

--- firmware/lib/task/stage2_hal.h
+++ firmware/lib/task/stage2_hal.h
@@ -22,6 +22,56 @@
 
 #include <machine.h>
 
+//#define RF_FAN 2
+//#define _HEATER 3
+//#define _STACK DAC0
+// This should change to PIN 5 when
+// we get the planned control board.
+//#define MAX31855_DATA_PIN 5
+//#define MAX31850_DATA_PIN 5
+// #define RF_FAN_TACH 5
+//#define RF_MOSTPLUS_FLOW_PIN A0
+//#define RF_MOSTPLUS_FLOW_LOW_CUTOFF_VOLTAGE 1.75
+
+#define BEEPER_PIN 37 // buzzer pin
+//#define U8_MISO 50
+//#define U8_MOSI 51
+//#define U8_SCK  52
+//#define SDSS   53 //sd card ss select
+//#define CD     49  //sd card card detect
+/*
+A0	FAN1_FG			Input		Blower 1 Tachometer	
+D4	TEMP1			BIDirect	Dallas One-Wire connection to Thermocouple Breakouts	Daisy chain connection to temprature probes. Address of ???
+D9	nFAN1_PWM		Output		Blower PWM	This output will be inverted
+D18	TX1	Output		Digital 	Power Supply TF800 Pin 23	This is Serial1 TX for (power supply)[https://assets.alliedelec.com/v1560852133/Datasheets/1d230174086e96b6e4801d1c963649f3.pdf]
+D19	RX1	Input		Digital 	Power Supply TF800 Pin 24	This is Serial1 RX for (power supply) [https://assets.alliedelec.com/v1560852133/Datasheets/1d230174086e96b6e4801d1c963649f3.pdf]
+D22	BLOWER_ENABLE	Output		Blower Enable	
+D32	GPAD_nCS		Output		External SPI inverted select (for the GPAD)	
+D44	LPBK0			Output		Varying loopback signal	
+D45	LPBK1			Input		Read of digital loopback signal	
+D51	HEAT1			Output		Positive SSR signal
+D50 Heat2			Output		Positive SSR signal for heaterPIDTask
+D49 HEAT3			Output		Positive SSR signal for heater PID 
+*/
+#define U8_DC 31  //LCD A0  
+#define U8_CS 48 //D0LCD_CS
+#define U8_RST 38 //LCD_RESET_PIN  
+#define U8_Width 128
+#define U8_Height 64
+
+//#rst and kill are both pull down can be used to reset mcu. 
+//GPIO Defines
+#define encA               40
+#define encB               24
+#define encButton          39 
+#define LED_BUILTIN_RED    37 //active high
+#define LED_BUILTIN_GREEN  23 //active high
+#define LED_BUILTIN_BLUE   53 //active high
+// This is the order in which the thermocouples are wired;
+// in a perfect world we might use device address
+//#define POST_STACK_0_IDX 0
+//#define POST_HEATER_0_IDX 1
+
 class Stage2HAL : public MachineHAL {
 public:
   bool init() override;
--- firmware/test/test_stage2_heater/stage2_heater.cpp
+++ firmware/test/test_stage2_heater/stage2_heater.cpp
@@ -35,7 +35,7 @@ using namespace OxCore;
 
 #include <Arduino.h>
 
-// #include <MAX31850.h>
+
 #include <duty_cycle_task.h>
 #include <heater_pid_task.h>
 #include <read_temps_task.h>
@@ -50,40 +50,6 @@ using namespace OxCore;
 #include <menuIO/clickEncoderIn.h>
 using namespace Menu;
 
-#define BEEPER_PIN 37 // buzzer pin
-//#define U8_MISO 50
-//#define U8_MOSI 51
-//#define U8_SCK  52
-//#define SDSS   53 //sd card ss select
-//#define CD     49  //sd card card detect
-/*
-A0	FAN1_FG			Input		Blower 1 Tachometer	
-D4	TEMP1			BIDirect	Dallas One-Wire connection to Thermocouple Breakouts	Daisy chain connection to temprature probes. Address of ???
-D9	nFAN1_PWM		Output		Blower PWM	This output will be inverted
-D18	TX1	Output		Digital 	Power Supply TF800 Pin 23	This is Serial1 TX for (power supply)[https://assets.alliedelec.com/v1560852133/Datasheets/1d230174086e96b6e4801d1c963649f3.pdf]
-D19	RX1	Input		Digital 	Power Supply TF800 Pin 24	This is Serial1 RX for (power supply) [https://assets.alliedelec.com/v1560852133/Datasheets/1d230174086e96b6e4801d1c963649f3.pdf]
-D22	BLOWER_ENABLE	Output		Blower Enable	
-D32	GPAD_nCS		Output		External SPI inverted select (for the GPAD)	
-D44	LPBK0			Output		Varying loopback signal	
-D45	LPBK1			Input		Read of digital loopback signal	
-D51	HEAT1			Output		Positive SSR signal
-D50 Heat2			Output		Positive SSR signal for heaterPIDTask
-D49 HEAT3			Output		Positive SSR signal for heater PID 
-*/
-#define U8_DC 31  //LCD A0  
-#define U8_CS 48 //D0LCD_CS
-#define U8_RST 38 //LCD_RESET_PIN  
-#define U8_Width 128
-#define U8_Height 64
-
-//#rst and kill are both pull down can be used to reset mcu. 
-//GPIO Defines
-#define encA               40
-#define encB               24
-#define encButton          39 
-#define LED_BUILTIN_RED    37 //active high
-#define LED_BUILTIN_GREEN  23 //active high
-#define LED_BUILTIN_BLUE   53 //active high
 
 U8X8_PCD8544_84X48_4W_HW_SPI u8x8(U8_CS, U8_DC , U8_RST);
 
